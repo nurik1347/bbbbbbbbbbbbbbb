b8ccf11aa133bfb813cf7b2dbf4544d8a6803f83
nurik1347 <nurikmirzaev1347@gmail.com>
2024-03-26 21:06:43 +0500

    done

---
+++ main.js
@@ -0,0 +1,11 @@
+let myConfirm = confirm("Savatga mahsulot qoshilsinmi")
+let arr = ['olma', 'ananas', 'apelsin']
+let product = 'mandarin'
+if(myConfirm === true) {
+    arr.push(product)
+    console.log(arr);
+}
+
+else {
+    console.log(arr);
+}
